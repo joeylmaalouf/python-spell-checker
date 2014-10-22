8b1f8eb71ca5c75f96eaf7195aa6beb480108319
Joey L. Maalouf <joeylmaalouf@gmail.com>
2014-10-22 15:11:37 -0400

    file not found error checking

--- spell_checker.py
+++ spell_checker.py
@@ -1,16 +1,26 @@
 # Joey L. Maalouf
 # Python Spell Checker
 
-# import our regex package for parsing our input
+# import our regex package for parsing our input, and the system package to quit
 import re
+import sys
 
-# -- get user input for filenames ----------------------------------------------
+
+def open_file(filename):
+    try:
+        f = open(filename)
+    except FileNotFoundError:
+        sys.exit("Sorry, no file was found with that name.")
+    return f
+
+# -- get user input ------------------------------------------------------------
+# Ask the user for the file names, and use the defaults if input is left blank.
 f1 = input(
-    "Please enter the filename of the word list. Leave blank for wordsEN.txt")
-f2 = input(
-    "Please enter the filename of the text to check. Leave blank for check.txt")
+    "Please enter the filename of the word list. Leave blank for wordsEN.txt: ")
 if f1 == '':
     f1 = 'wordsEN.txt'
+f2 = input(
+    "Please enter the filename to check. Leave blank for check.txt: ")
 if f2 == '':
     f2 = 'check.txt'
 
@@ -19,7 +29,7 @@ if f2 == '':
 # string in the list that evaluates to false (i.e. '') via list comprehensions.
 # Make everything lowercase for case-insensitive comparisons later, and add the
 # basic letters of the alphabet to the list as well.
-text_file = open(f1)
+text_file = open_file(f1)
 word_list = text_file.read().split('\n')
 word_list = [word.lower() for word in word_list if word]
 word_list.extend(list('abcdefghijklmnopqrstuvwxyz'))
@@ -27,7 +37,7 @@ word_list.extend(list('abcdefghijklmnopqrstuvwxyz'))
 # -- process the file of words to check ----------------------------------------
 # Open the file with words to check, then use regex with word boundaries
 # to get a list of all the individual words.
-check_file = open(f2)
+check_file = open_file(f2)
 to_check = re.findall(r"\b[a-zA-Z0-9]+\b", check_file.read())
 
 # -- check for any incorrect words ---------------------------------------------
@@ -47,5 +57,9 @@ for word in incorrect:
     if word not in output:
         output.append(word)
 
-# print out our final output
-print(output)
+# -- print out our final output ------------------------------------------------
+# For every word in the output, tell the user which word was incorrect, as well
+# as its numerical index (position) in the input file.
+for word in output:
+    print('Found unknown word \"%s\" at position %d' %
+          (word, to_check.index(word)))
