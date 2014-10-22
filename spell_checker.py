41e8b6c973be26cdf31eae4658b0d294ae4c61ae
Joey L. Maalouf <joeylmaalouf@gmail.com>
2014-10-22 18:45:10 -0400

    finalized the program
    (I hope)

--- spell_checker.py
+++ spell_checker.py
@@ -1,28 +1,34 @@
-# Joey L. Maalouf
-# Python Spell Checker
+# JLM - Python Spell Checker
+# Copyright (c) 2014 Joey Luke Maalouf
 
 # import our regex package for parsing our input, and the system package to quit
 import re
 import sys
 
 
+# -- attempt to open a file ----------------------------------------------------
+# Use the built-in 'open' command, with some error handling.
 def open_file(filename):
     try:
         f = open(filename)
     except FileNotFoundError:
-        sys.exit("Sorry, no file was found with that name.")
+        print('Sorry, no file was found with the name \'%s\'.\n' % filename +
+              'Please check your file\'s name and re-run the program.')
+        sys.exit(1);
     return f
 
 # -- get user input ------------------------------------------------------------
 # Ask the user for the file names, and use the defaults if input is left blank.
-f1 = input(
-    "Please enter the filename of the word list. Leave blank for wordsEN.txt: ")
+print('Text File Spell Checker');
+f1 = input('\nPlease enter the filename of the word list.\n' +
+           '    (Leave blank to use \'wordsEN.txt\'): ')
 if f1 == '':
     f1 = 'wordsEN.txt'
-f2 = input(
-    "Please enter the filename to check. Leave blank for check.txt: ")
+f2 = input('\nPlease enter the filename to check for incorrect spelling.\n' +
+           '    (Leave blank to use \'check.txt\'): ')
 if f2 == '':
     f2 = 'check.txt'
+print('\nChecking input file \'%s\' against list file \'%s\':' % (f1, f2))
 
 # -- process the file of acceptable words --------------------------------------
 # Open the file with acceptable words and read them into a list, then remove any
@@ -38,12 +44,12 @@ word_list.extend(list('abcdefghijklmnopqrstuvwxyz'))
 # Open the file with words to check, then use regex with word boundaries
 # to get a list of all the individual words.
 check_file = open_file(f2)
-to_check = re.findall(r"\b[a-zA-Z0-9]+\b", check_file.read())
+to_check = re.findall(r'\b[a-zA-Z0-9]+\b', check_file.read())
 
 # -- check for any incorrect words ---------------------------------------------
 # For every word we found in the input file, if the word to check is neither in
 # our acceptable list nor a pure number, add it to the list of incorrect words.
-# This way, we can mark both 'phone' and '4' as correct, but not 'phone4'.
+# This way, we can mark both 'phone' and '75' as correct, but not 'phone75'.
 incorrect = []
 for word in to_check:
     if not (word.lower() in word_list or word.isnumeric()):
@@ -61,5 +67,6 @@ for word in incorrect:
 # For every word in the output, tell the user which word was incorrect, as well
 # as its numerical index (position) in the input file.
 for word in output:
-    print('Found unknown word \"%s\" at position %d' %
+    print('Found unknown word \'%s\' at position %d.' %
           (word, to_check.index(word)))
+print('\nEnd of program.\n')
